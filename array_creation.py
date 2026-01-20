8a9092d8d842ea2148a883897d5615ee5bfc7fd8
dinabakhtiari <142736449+dinabakhtiari@users.noreply.github.com>
2026-01-20 15:58:52 +0000

    2d arrays

--- array_creation.py
+++ array_creation.py
@@ -1,3 +1,6 @@
 import numpy as np
-my_array = np.array([10, 9, 8])
-print(my_array)
+#my_array = np.array([10, 9, 8])
+#print(my_array)
+
+my_2d_array = np.array([[1,3,5],[2,4,6]])
+print(my_2d_array[1, 1])
