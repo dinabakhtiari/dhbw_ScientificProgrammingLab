d9947bc1442bdd52196ed262440f72c3175e94dd
dinabakhtiari <142736449+dinabakhtiari@users.noreply.github.com>
2026-01-20 16:15:45 +0000

    playing with arrays 3

--- array_creation.py
+++ array_creation.py
@@ -4,14 +4,27 @@ my_array = np.array([10, 9, 8])
 
 my_2d_array = np.array([[1, 3, 5, 7],[2, 4, 6, 8]], dtype=np.float32)
 print(my_2d_array[1, 1])
-print("-------------")
+print("---------------")
 print(my_2d_array[1])
-print("-------------")
+print("---------------")
 print(my_2d_array[:, 2])
-print("-------------")
+print("---------------")
 print(my_array.shape)
 print(my_2d_array.shape)
-print("-------------")
+print("---------------")
 print(my_2d_array.dtype)
-print("-------------")
+print("---------------")
 
+ones_array = np.ones((3, 5))
+print(ones_array)
+print("---------------")
+
+print(np.arange(2, 4))
+print("---------------")
+
+print(np.linspace(2, 3, 18))
+print("---------------")
+
+
+print(my_2d_array.reshape(4, 2))
+print("---------------")
