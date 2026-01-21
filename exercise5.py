a885b173b04cf358196eca9990e505097ca55575
Dina Bakhtiari <dinaba701@gmail.com>
2026-01-21 16:46:12 +0100

    5th exercise - use np.maximum

---
+++ exercise5.py
@@ -0,0 +1,12 @@
+import numpy as np
+
+def relu(x):
+
+#   return np.maximum(0, x)
+  return np.maximum(0, x)  
+
+#print(relu(10))
+print(relu(5))
+
+arr = np.array([1, -2, 3, -4])
+print("Array result:", relu(arr))
