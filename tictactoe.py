bf4515392cc7ce03e867dfc752239cfb877d1167
Dina Bakhtiari <dinaba701@gmail.com>
2026-02-02 17:01:15 +0100

    add win detection and tie-checking logic

--- tictactoe.py
+++ tictactoe.py
@@ -5,5 +5,22 @@ def print_board(board):
             print("-----------")
 
 board = [[' ' for _ in range(3)] for _ in range(3)]
+
+def check_winner(b):
+    for i in range(3):
+        if b[i][0] == b[i][1] == b[i][2] and b[i][0] != ' ': return b[i][0]
+        if b[0][i] == b[1][i] == b[2][i] and b[0][i] != ' ': return b[0][i]
+    if b[0][0] == b[1][1] == b[2][2] and b[0][0] != ' ': return b[0][0]
+    if b[0][2] == b[1][1] == b[2][0] and b[0][2] != ' ': return b[0][2]
+    
+    if all(cell != ' ' for row in b for cell in row): return 'Tie'
+    return None
+
+print_board(board)
+board[0][0] = 'X'
+board[0][1] = 'X'
+board[0][2] = 'X'
 print_board(board)
 
+winner = check_winner(board)
+print(f"Result {winner}")
