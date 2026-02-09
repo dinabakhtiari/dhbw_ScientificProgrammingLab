d971c4dee1f25a5c2a090e659750f5abea77e0f0
Dina Bakhtiari <dinaba701@gmail.com>
2026-02-09 13:56:09 +0100

    see the data types and missing datas

--- eda.py
+++ eda.py
@@ -9,3 +9,9 @@ print(df.shape)
 
 print("\nFirst 5 rows:")
 print(df.head())
+
+print("\nData Types and Missing Values:")
+print(df.info())
+# Total Number of Rows: 344 (RangeIndex: 344 entries).
+# There are 2 missing rows in the columns related to bill measurements and body mass (bill_length_mm, bill_depth_mm, flipper_length_mm, and body_mass_g), leaving 342 non-null entries out of a total of 344
+# The 'sex' column contains 11 missing rows, with 333 non-null entries out of 344 total.
