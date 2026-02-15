1fd48d307708a8393e677f8d61cee24ba7a03d6c
Dina Bakhtiari <dinaba701@gmail.com>
2026-02-15 17:19:58 +0100

    when is the best time for buying and selling a house?

--- EDA_Project/hypo2.py
+++ EDA_Project/hypo2.py
@@ -6,8 +6,29 @@ import matplotlib.pyplot as plt
 df = pd.read_csv('EDA_Project/KC_housing_data.csv')
 
 df['date'] = pd.to_datetime(df['date'])
-df['year'] = df['date'].dt.year
 df['month'] = df['date'].dt.month
 
-# بررسی مقادیر خالی (Missing Values)
-print(df.isnull().sum())
+# Missing Values
+# print(df.isnull().sum())
+
+monthly_trend = df.groupby('month')['price'].mean().reset_index()
+
+plt.figure(figsize=(10, 6))
+sns.lineplot(data=monthly_trend, x='month', y='price', marker='o', color='red', linewidth=3)
+
+plt.title('Best Time to Buy or Sell (Price Trend)', fontsize=15)
+plt.xlabel('Month', fontsize=12)
+plt.ylabel('Average Price ($)', fontsize=12)
+plt.xticks(range(1, 13), ['Jan', 'Feb', 'Mar', 'Apr', 'May', 'Jun', 'Jul', 'Aug', 'Sep', 'Oct', 'Nov', 'Dec'])
+plt.grid(True, linestyle='--', alpha=0.6)
+
+plt.show()
+
+print("Monthly Average Price Trend:")
+print(monthly_trend)
+
+# تحلیل بر اساس نمودار:
+#در بازه زمانی موجود در فایل شما (می تا جولای ۲۰۱۴)، روند قیمت‌ها کاملاً صعودی است.
+#بهترین زمان برای خرید (Buying):
+#ماه می (May): طبق نمودار، پایین‌ترین میانگین قیمت مربوط به ماه می است (حدود ۵۴۰ هزار دلار). بنابراین برای خریداری که به دنبال قیمت کمتر است، شروع بازه زمانی (ماه می) بهترین زمان بوده است.
+#بهترین زمان برای فروش (Selling): ماه جولای (July): نمودار نشان می‌دهد که قیمت‌ها به شدت افزایش یافته و در ماه جولای به بالاترین حد خود رسیده‌اند (حدود ۶۱۹ هزار دلار). بنابراین برای فروشندگان، جولای بهترین ماه برای کسب سود بیشتر است.
